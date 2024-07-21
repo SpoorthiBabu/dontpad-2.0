bc9b4c0c4be1123f97db8aa2c499c50d9de6123d
SpoorthiBabu <babuspoorthi6@gmail.com>
2024-07-22 02:32:32 +0530

    i can see the text on the dynamic page but not the image:(

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from pymongo import MongoClient
 import gridfs
-from image_utils import save_image
+from io import BytesIO
+from bson.objectid import ObjectId
+from image_utils import save_image, get_content, save_text
 
 app = Flask(__name__)
 
@@ -13,27 +15,45 @@ fs = gridfs.GridFS(db)
 # def index():
 #     return render_template('index.html')
 
-# @app.route('/<page_name>', methods=['GET', 'POST'])
-# def page(page_name):
-#     if request.method == 'POST':
-#         if 'file' in request.files:
-#             file = request.files['file']
-#             save_image(db, file.read(), page_name)
-#         if 'text' in request.form:
-#             # Save text content to the database
-#             pass
-
-#     content = get_content(db, page_name)
-#     return render_template('page.html', page_name=page_name, content=content)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        page_name = request.form.get('page_name')
+        return redirect(url_for('page', page_name=page_name))
+    return render_template('index.html')
+
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         if 'file' in request.files:
+#             file = request.files['file']
+#             save_image(db, file.read(), 'uploaded_image')
+#             return 'Image uploaded successfully!'
+#     return render_template('index.html')
+
+
+@app.route('/<page_name>', methods=['GET', 'POST'])
+def page(page_name):
+    if request.method == 'POST':
         if 'file' in request.files:
             file = request.files['file']
-            save_image(db, file.read(), 'uploaded_image')
-            return 'Image uploaded successfully!'
-    return render_template('index.html')
+            save_image(db, file.read(), page_name)
+        if 'text' in request.form:
+            text = request.form.get('text')
+            save_text(db, text, page_name) 
+
+    content = get_content(db, page_name)
+    return render_template('page.html', page_name=page_name, content=content)
+
+@app.route('/image/<file_id>')
+def get_image(file_id):
+    try:
+        file = fs.get(ObjectId(file_id))
+        return send_file(BytesIO(file.read()), mimetype=file.content_type)
+    except gridfs.errors.NoFile:
+        return "File not found", 404
 
 
 if __name__ == '__main__':
--- image_utils.py
+++ image_utils.py
@@ -17,8 +17,40 @@
 
 import gridfs
 from pymongo import MongoClient
+from datetime import datetime
 
-def save_image(db, image_bytes, filename):
+
+# def save_image(db, image_bytes, filename):
+#     fs = gridfs.GridFS(db)
+#     fs.put(image_bytes, filename=filename)
+
+def save_image(db, image_bytes, page_name):
     fs = gridfs.GridFS(db)
-    fs.put(image_bytes, filename=filename)
+    file_id = fs.put(image_bytes, filename=f"{page_name}_{datetime.now()}", content_type="image/jpeg")
+    return file_id
 
+def save_text(db, text, page_name):
+    texts_collection = db['texts']
+    texts_collection.insert_one({'page_name': page_name, 'text': text, 'timestamp': datetime.now()})
+
+def get_content(db, page_name):
+    fs = gridfs.GridFS(db)
+    content = []
+    
+    # Get images
+    for file in fs.find({"filename": {"$regex": f"^{page_name}_"}}):
+        content.append({
+            "type": "image",
+            "content": fs.get(file._id).read()
+        })
+
+    # Get text
+    texts_collection = db['texts']
+    texts = texts_collection.find({"page_name": page_name}).sort("timestamp", -1)
+    for text in texts:
+        content.append({
+            "type": "text",
+            "content": text['text']
+        })
+
+    return content
